d0b74e0c126ed5fea31d343e51a220db09a65c59
Brian Holt <btholt@gmail.com>
2025-10-26 16:36:38 -0700

    styled

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,8 @@
 import Head from "next/head";
 import "@fortawesome/fontawesome-free/css/all.css";
-
-import "highlight.js/styles/a11y-light.css";
 import "../styles/variables.css";
 import "../styles/footer.css";
 import "../styles/courses.css";
-
 import Layout from "../components/layout";
 
 export default function App({ Component, pageProps }) {
